93d5c83a0ebe010b65540ba8f3d27ad5b75cbb36
Anthony Santos <anthony.2226191@aluno.faculdadedescomplica.com.br>
2023-09-19 12:35:50 -0300

    primeiro commit

---
+++ collections_java_api_2023/src/collections_java_api_2023/main.java
@@ -0,0 +1,15 @@
+package collections_java_api_2023;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello ans Welcome");
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("i = " + i);
+		}
+
+	}
+
+}
