24001c71b1b58b484d7aaaae808716cf8cb6a044
Anthony Santos <anthony.2226191@aluno.faculdadedescomplica.com.br>
2023-10-15 16:43:35 -0300

    primeiro commit do projeto

---
+++ Project-API-REST/src/test/java/me/dio/Project/APIREST/ProjectApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Project.APIREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
